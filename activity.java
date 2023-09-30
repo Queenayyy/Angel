94e8cf6005958b49f50e990df4be0e640cde8a0d
EllaineMangampo <142650687+Queenayyy@users.noreply.github.com>
2023-09-30 13:37:49 +0800

    Ellaine-commit

---
+++ activity.java
@@ -0,0 +1,30 @@
+package activity;
+
+public class activity {
+
+  public static void main(String[] args) {
+  
+  boolean bool= true;
+  boolean bool= false;
+   
+   int val = (bool)? 1:0;
+   int val2 = (bool2)? 1:0;
+   System.out.print(val);
+   System.out.print(val2);
+   System.out.print(val);
+   System.out.print(val2);
+   System.out.print(val);
+   System.out.print(val2);
+   System.out.print(val);
+   System.out.print(val2);
+   System.out.println();
+   System.out.print(val2);
+   System.out.print(val2);
+   System.out.print(val2);
+   System.out.print(val2);
+   System.out.print(val);
+   System.out.print(val);
+   System.out.print(val);
+   System.out.print(val); 
+  }
+}
